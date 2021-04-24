e6c34937dc0e99b3f357ad3f2886716edf8ce795
Audrey <66935319+AudreyLau8@users.noreply.github.com>
2021-04-23 22:41:41 -0700

    Update main.py
    shrunk and translated inverted snowflake to the middle of black snowflake

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def invKoch(sideLength, level):
   if level > 0:
     for angle in [60, -120, 60, 0]:
       tommy.color(randint(0,255),randint(0,255),randint(0,255))
-      koch(sideLength/3, level-1) #replace this with a recursive call to koch
+      koch(sideLength/4, level-1) #replace this with a recursive call to koch
       tommy.left(angle)
   else:
        tommy.forward(sideLength)
@@ -37,6 +37,9 @@ def invFlake(sideLength, level):
 tommy.begin_fill()
 snowflake(300,4)
 tommy.end_fill()
+tommy.penup()
+tommy.goto((35+40)/2, -150)
+tommy.pendown()
 invFlake(300,4)
 tommy.right(90)
 turtle.update()
